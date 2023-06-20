d0f2dd2f4cc1e7f5ae3e54b141a76f26f5a84abf
YadaYuki <yada.yuki@fuji.waseda.jp>
2023-06-20 19:52:33 +0900

    fix:fixed nDCG interface

--- src/utils/metrics/metrics.py
+++ src/utils/metrics/metrics.py
@@ -1,28 +1,19 @@
-import math
+import numpy as np
 
 
 def nDCG_K(
-    item_ids_to_relevance_scores_map: dict[str, float],
-    ordered_recommend_items: list[str],
+    recommended_top_Kth_items_scores: list[float],
     K: int,
-    number_of_positive: int,
 ) -> float:
-    return DCG_K(item_ids_to_relevance_scores_map, ordered_recommend_items, K) / sum(
-        [1 / math.log2(1 + (i + 1)) for i in range(number_of_positive)]
-    )
+    return DCG_K(recommended_top_Kth_items_scores, K) / DCG_K(np.ones(K).tolist(), K)
 
 
 def DCG_K(
-    item_ids_to_relevance_scores_map: dict[str, float],
-    ordered_recommend_items: list[str],
+    recommended_top_Kth_items_scores: list[float],
     K: int,
 ) -> float:
-    assert len(item_ids_to_relevance_scores_map) == K
-    assert len(ordered_recommend_items) == K
-
-    return sum(
-        [
-            (2 ** item_ids_to_relevance_scores_map[item] - 1) / math.log2(1 + (i + 1))
-            for i, item in enumerate(ordered_recommend_items)
-        ]
-    )
+    assert len(recommended_top_Kth_items_scores) == K
+    Kth_items_scores = np.array(recommended_top_Kth_items_scores)
+    gains = 2**Kth_items_scores - 1
+    discounts = np.log2(np.arange(1, K + 1) + 1)
+    return sum(gains / discounts)
--- test/utils/test_metrics.py
+++ test/utils/test_metrics.py
@@ -3,32 +3,28 @@ from utils.metrics.metrics import DCG_K, nDCG_K
 
 
 @pytest.mark.parametrize(
-    "item_ids_to_relevance_scores_map,ordered_recommended_items,K,expected",
+    "recommended_item_scores,K,expected",
     [
-        ({"item_a": 1.0, "item_b": 0.0, "item_c": 1.0}, ["item_a", "item_b", "item_c"], 3, 1.5),
-        ({"item_a": 1.0, "item_b": 1.0, "item_c": 1.0}, ["item_a", "item_b", "item_c"], 3, 2.1309),
-        ({"item_a": 0.0, "item_b": 0.0, "item_c": 0.0}, ["item_a", "item_b", "item_c"], 3, 0.0),
+        ([1.0, 0.0, 1.0], 3, 1.5),
+        ([1.0, 1.0, 1.0], 3, 2.1309),
+        ([0.0, 0.0, 0.0], 3, 0.0),
     ],
 )
-def test_DCG_at_K(
-    item_ids_to_relevance_scores_map: dict[str, float], ordered_recommended_items: list[str], K: int, expected: float
-) -> None:
-    assert DCG_K(item_ids_to_relevance_scores_map, ordered_recommended_items, K) == pytest.approx(expected, 0.001)
+def test_DCG_at_K(recommended_item_scores: list[float], K: int, expected: float) -> None:
+    assert DCG_K(recommended_item_scores, K) == pytest.approx(expected, 0.001)
 
 
 @pytest.mark.parametrize(
-    "item_ids_to_relevance_scores_map,ordered_recommended_items,K,N,expected",
+    "recommended_item_scores,K,expected",
     [
-        ({"item_a": 1.0, "item_b": 0.0, "item_c": 1.0}, ["item_a", "item_b", "item_c"], 3, 5, 0.5087),
-        ({"item_a": 1.0, "item_b": 1.0, "item_c": 1.0}, ["item_a", "item_b", "item_c"], 3, 5, 0.7227),
-        ({"item_a": 0.0, "item_b": 0.0, "item_c": 0.0}, ["item_a", "item_b", "item_c"], 3, 5, 0.0),
+        ([1.0, 0.0, 1.0], 3, 0.7039),
+        ([1.0, 1.0, 1.0], 3, 1),
+        ([0.0, 0.0, 0.0], 3, 0.0),
     ],
 )
 def test_nDCG_at_K(
-    item_ids_to_relevance_scores_map: dict[str, float],
-    ordered_recommended_items: list[str],
+    recommended_item_scores: list[float],
     K: int,
-    N: int,
     expected: float,
 ) -> None:
-    assert nDCG_K(item_ids_to_relevance_scores_map, ordered_recommended_items, K, N) == pytest.approx(expected, 0.001)
+    assert nDCG_K(recommended_item_scores, K) == pytest.approx(expected, 0.001)
