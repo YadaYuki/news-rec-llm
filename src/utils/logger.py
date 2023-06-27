99c04e7c5d8cbe38cae7a265ddce94b0e2ca9b20
YadaYuki <yada.yuki@fuji.waseda.jp>
2023-06-28 01:34:35 +0900

    feat: add RecommenderBase

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,5 +1,3 @@
 import logging
 
-logging.basicConfig(level=logging.INFO)
-
-logger = logging.getLogger("news rec llm")
+logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s")
