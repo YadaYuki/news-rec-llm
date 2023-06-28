4105576e515786dc5fc4136ef4cb6db95ca28b8b
YadaYuki <yada.yuki@fuji.waseda.jp>
2023-06-28 20:33:30 +0900

    feat: add read_behavior_df

--- src/utils/dataset/MINDDataFrame.py
+++ src/utils/dataset/MINDDataFrame.py
@@ -6,7 +6,7 @@ from pathlib import Path
 class MINDDataFrame:
     def read_news_df(self, path_to_news_tsv: Path, has_entities: bool = False) -> pl.DataFrame:
         # FIXME:
-        # pl.read_csvを直接実行すると行が欠損するのでpandasから読み取る形式をとる。
+        # pl.read_csvを直接実行すると、行が欠損するため、pandasでtsvを読み取り、polarsのDataFrameに変換する方式にする。
         news_df = pd.read_csv(path_to_news_tsv, sep="\t", encoding="utf8", header=None)
         news_df.columns = [
             "news_id",
@@ -23,3 +23,34 @@ class MINDDataFrame:
         if has_entities:
             return news_df
         return news_df.drop("title_entities", "abstract_entities")
+
+    def read_behavior_df(self, path_to_behavior_tsv: Path) -> pl.DataFrame:
+        behavior_df = pl.read_csv(path_to_behavior_tsv, separator="\t", encoding="utf8-lossy", has_header=False)
+        behavior_df = behavior_df.rename(
+            {
+                "column_1": "impression_id",
+                "column_2": "user_id",
+                "column_3": "time",
+                "column_4": "history",
+                "column_5": "impressions",
+            }
+        )
+        behavior_df = behavior_df.with_columns((pl.col("impressions").str.split(" ")).alias("impression_news_list"))
+        behavior_df = behavior_df.explode(pl.col("impression_news_list"))
+        behavior_df = behavior_df.with_columns(
+            [
+                (pl.col("impression_news_list").str.split("-").list.get(0)).alias("news_id"),
+                (pl.col("impression_news_list").str.split("-").list.get(1).str.parse_int()).alias("clicked"),
+            ]
+        ).select(
+            [
+                "impression_id",
+                "user_id",
+                "news_id",
+                "time",
+                "history",
+                "clicked",
+            ]
+        )
+
+        return behavior_df
--- test/utils/dataset/test_MINDDataFrame.py
+++ test/utils/dataset/test_MINDDataFrame.py
@@ -1,16 +1,16 @@
-from src.const.path import DATASET_DIR
+from src.const.path import MIND_SMALL_VAL_DATASET_DIR, MIND_SMALL_TRAIN_DATASET_DIR
 from src.utils.dataset.MINDDataFrame import MINDDataFrame
 
 
 class TestMINDDataFrame:
     def test_read_news_df(self) -> None:
-        small_val_news_tsv_path = DATASET_DIR / "mind" / "small" / "val" / "news.tsv"
+        small_val_news_tsv_path = MIND_SMALL_VAL_DATASET_DIR / "news.tsv"
         news_df = MINDDataFrame().read_news_df(small_val_news_tsv_path)
         assert news_df.columns == ["news_id", "category", "subcategory", "title", "abstract", "url"]
         assert len(news_df) == 42416
 
     def test_read_news_df_with_entities(self) -> None:
-        small_val_news_tsv_path = DATASET_DIR / "mind" / "small" / "val" / "news.tsv"
+        small_val_news_tsv_path = MIND_SMALL_VAL_DATASET_DIR / "news.tsv"
         news_df = MINDDataFrame().read_news_df(small_val_news_tsv_path, has_entities=True)
         assert news_df.columns == [
             "news_id",
@@ -23,3 +23,15 @@ class TestMINDDataFrame:
             "abstract_entities",
         ]
         assert len(news_df) == 42416
+
+    def test_read_behavior_df(self) -> None:
+        small_val_behavior_tsv_path = MIND_SMALL_TRAIN_DATASET_DIR / "behaviors.tsv"
+        behavior_df = MINDDataFrame().read_behavior_df(small_val_behavior_tsv_path)
+        assert behavior_df.columns == [
+            "impression_id",
+            "user_id",
+            "news_id",
+            "time",
+            "history",
+            "clicked",
+        ]
