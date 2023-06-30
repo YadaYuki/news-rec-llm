c590c62bdeae48af66247a526ad0b180ce42f830
YadaYuki <yada.yuki@fuji.waseda.jp>
2023-06-30 15:56:16 +0900

    feat: add fit & recommend_top_k_items

--- src/recommendation/NewsRecommenderBase.py
+++ src/recommendation/NewsRecommenderBase.py
@@ -1,19 +1,20 @@
 import polars as pl
 import numpy as np
 from abc import ABC, abstractmethod
+from typing import Tuple
 
 
 class RecommenderBase(ABC):
     @abstractmethod
-    def fit(self, train: pl.DataFrame) -> None:
+    def fit(self, train_behavior_df: pl.DataFrame, train_news_df: pl.DataFrame) -> None:
         pass
 
     @abstractmethod
     def recommend_top_k_items(
-        self, user_ids: np.ndarray, behavior_df: pl.DataFrame, news_df: pl.DataFrame
-    ) -> pl.DataFrame:
+        self, user_ids: np.ndarray, val_behavior_df: pl.DataFrame, val_news_df: pl.DataFrame, K: int
+    ) -> Tuple[pl.DataFrame, pl.DataFrame]:
         pass
 
     @abstractmethod
-    def predict_score(self, input: pl.DataFrame) -> pl.DataFrame:
+    def predict_score(self, val_behavior_df: pl.DataFrame, val_news_df: pl.DataFrame) -> np.ndarray:
         pass
