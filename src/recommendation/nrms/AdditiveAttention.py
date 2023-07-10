9cc638ddeaa4bb2e9476b43ac1c8cb84bef2ef4c
YadaYuki <yada.yuki@fuji.waseda.jp>
2023-07-10 18:23:07 +0900

    feat: add attention

--- src/recommendation/nrms/AdditiveAttention.py
+++ src/recommendation/nrms/AdditiveAttention.py
@@ -6,10 +6,14 @@ class AdditiveAttention(nn.Module):
     def __init__(self, input_dim: int, hidden_dim: int) -> None:
         super().__init__()
         self.attention = nn.Sequential(
-            nn.Linear(input_dim, hidden_dim),  # V
-            nn.Tanh(),
-            nn.Linear(hidden_dim, 1),
+            nn.Linear(
+                input_dim, hidden_dim
+            ),  # in: (batch_size, seq_len, input_dim), out: (batch_size, seq_len, hidden_dim)
+            nn.Tanh(),  # in: (batch_size, seq_len, hidden_dim), out: (batch_size, seq_len, hidden_dim)
+            nn.Linear(hidden_dim, 1),  # in: (batch_size, seq_len, hidden_dim), out: (batch_size, seq_len, 1)
+            nn.Softmax(dim=-2),
         )
 
     def forward(self, input: torch.Tensor) -> torch.Tensor:
-        return torch.rand(input.shape)
+        attention_weight = self.attention(input)
+        return input * attention_weight
