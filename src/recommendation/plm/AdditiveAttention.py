0fc3bc155b95936debfbdcf9e1dae939c538048c
YadaYuki <yada.yuki@fuji.waseda.jp>
2023-07-10 17:07:01 +0900

    feat: definite AdditiveAttention

---
+++ src/recommendation/plm/AdditiveAttention.py
@@ -0,0 +1,10 @@
+import torch
+from torch import nn
+
+
+class AdditiveAttention(nn.Module):
+    def __init__(self) -> None:
+        pass
+
+    def forward(self) -> torch.Tensor:
+        return torch.Tensor()
